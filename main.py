403667223d07e79718948f123c2743b5e4c62406
sullhouse <sullhouse@gmail.com>
2024-10-16 21:41:35 -0400

    Test commit for CD

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def hello_http(request):
             "json": request_json
         }
 
-        # Upload the entire request data in a readable format
+        # Upload the entire request data to GCS bucket in a readable format
         blob.upload_from_string(json.dumps(request_data, indent=2))
     except Exception as e: 
         return f"An error occurred while trying to save request to GCS: {e}"
